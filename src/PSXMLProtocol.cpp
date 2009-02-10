6a11e78ff6e1c3ed62ec6a35a764d3a02c679856
David Hinkes <dh83@localhost.(none)>
2009-02-09 18:13:50 -0800

    PSXMLProtocol fixed xmlpp Document crash
    As it turns out, xmlpp parser maintains a Document internally.
    When the parser parses a new xml stream, it creates a new
    instance of the internal Document, and deletes the previous one.
    Therefore, the xmlpp Documents don't exist are gone by the time
    the XML is returned from the XMLPSPrococol object.
    
    To address this issue, the XMLPSProtocol object creates a new
    object for each incoming XML message.  The object is allocated
    on the heap and resource managed by boost's shared_ptr.  Hence
    the API changed to include boost shared pointers.

--- src/PSXMLProtocol.cpp
+++ src/PSXMLProtocol.cpp
@@ -1,9 +1,18 @@
 #include "PSXMLProtocol.h"
 
 #include <arpa/inet.h>
+#include <cassert>
+
 using namespace std;
 using namespace psxml;
 using namespace xmlpp;
+using namespace boost;
+
+// module local helper structure, PSXML Protocol's header frame
+struct __psxml_header_t {
+  char id[8];
+  uint32_t size;
+};
 
 unsigned int PSXMLProtocol::_process_frame(const char * in,unsigned int size) {
   unsigned int read = 0;
@@ -20,15 +29,23 @@ unsigned int PSXMLProtocol::_process_frame(const char * in,unsigned int size) {
       // do the xml parsing
       _parser.parse_memory_raw(reinterpret_cast<const unsigned char *>(
         in+offset+12),payload_size);
-      _decoder_output.push_back(_parser.get_document()->get_root_node());
-      // we just processed a whole bunch of bytes
-      read = offset+12+payload_size;
+      // if the XML was parsed correctly, append it to the vector
+      if(_parser) {
+        shared_ptr<Document> doc(new Document());
+        doc->create_root_node_by_import(
+	  _parser.get_document()->get_root_node());
+        _decoder_output.push_back(doc);
+        // we just processed a whole bunch of bytes
+        read = offset+12+payload_size;
+      } else {
+      }
     }
   }
   return read;
 }
 
-vector<Element*> PSXMLProtocol::decode(const char * in, unsigned int size) {
+vector<shared_ptr<Document> > PSXMLProtocol::decode(const char * in,
+  unsigned int size) {
   // 1) Append start buffer to the end of the decder vector
   // make the vector big enough to handle the data
   unsigned int vector_size = _decoder_residual.size();
@@ -56,4 +73,30 @@ vector<Element*> PSXMLProtocol::decode(const char * in, unsigned int size) {
  
   // 5) we're done
   return _decoder_output;
-} 
+}
+
+unsigned int PSXMLProtocol::pull_encoded_size() const {
+  return _encoder_residual.size();
+}
+
+const char * PSXMLProtocol::pull_encoded() {
+  return &_encoder_residual[0];
+}
+
+void PSXMLProtocol::pull_encoded(unsigned int bytes) {
+  // if bytes are > residual's size, something is wrong
+  unsigned int s = _encoder_residual.size();
+  assert(bytes <= s);
+  memcpy(&_encoder_residual[0], &_encoder_residual[0]+bytes,s-bytes);
+  _encoder_residual.resize(s-bytes);
+}
+
+void PSXMLProtocol::encode(shared_ptr<Document> in) {
+  unsigned int s = _encoder_residual.size();
+  Glib::ustring out = in->write_to_string();
+  _encoder_residual.resize(s+out.bytes()+sizeof(__psxml_header_t));
+  __psxml_header_t h = { "psxml",htonl(out.bytes()) };
+  memcpy(&_encoder_residual[0]+s,&h,sizeof(__psxml_header_t));
+  memcpy(&_encoder_residual[0]+s+sizeof(__psxml_header_t),
+    out.data(),out.bytes());
+}
--- src/PSXMLProtocol.h
+++ src/PSXMLProtocol.h
@@ -8,6 +8,7 @@
 
 #include <vector>
 #include <libxml++/libxml++.h>
+#include <boost/shared_ptr.hpp>
 
 namespace psxml {
   class PSXMLProtocol {
@@ -16,11 +17,12 @@ namespace psxml {
      * inject bytes off-the-wire into the decoder and return a list
      * of PSXML messages
      */
-    std::vector<xmlpp::Element*> decode(const char * in, unsigned int size);
+    std::vector<boost::shared_ptr<xmlpp::Document> > decode(
+      const char * in, unsigned int size);
     /*
      * inject PSXML message to be serialized
      */
-    void encode(xmlpp::Element * in);
+    void encode(boost::shared_ptr<xmlpp::Document> in);
     /*
      * get the number of bytes ready to be outputted from the encoder
      */
@@ -30,7 +32,7 @@ namespace psxml {
      * Note: the char * lives within the PXMLPotocol object, no
      * need to delete after use.
      */
-    char * pull_encoded();
+    const char * pull_encoded();
 
     /*
      * tell the encoder how may bytes were successfully removed
@@ -40,7 +42,7 @@ namespace psxml {
     unsigned int _process_frame(const char * in, unsigned int size);
     std::vector<char> _decoder_residual;
     std::vector<char> _encoder_residual;
-    std::vector<xmlpp::Element *> _decoder_output;
+    std::vector<boost::shared_ptr<xmlpp::Document> > _decoder_output;
     xmlpp::DomParser _parser;
   };
 }
