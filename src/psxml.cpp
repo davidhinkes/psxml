449e3ac20593b3e8a56465175a61a9347e4de3f4
David Hinkes <dh83@localhost.(none)>
2009-02-15 23:40:21 -0800

    progress on the psxml client, need to re-think how to return xml to the user

--- src/psxml.cpp
+++ src/psxml.cpp
@@ -9,9 +9,12 @@
 
 #include <cassert>
 
+#include <sys/select.h>
+
 using namespace psxml;
 using namespace xmlpp;
 using namespace std;
+using namespace boost;
 
 Connection::Connection(const std::string & url,unsigned short port) {
   _fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -39,7 +42,64 @@ void Connection::_send_socket_io() {
   }
   _protocol.pull_encoded(sent);
 }
+void Connection::subscribe(const list<XPathExpression> & xpaths) {
+  Document doc;
+  Element * root = doc.create_root_node("Subscribe",
+    "http://www.psxml.org/PSXML-0.1","psx");
+  for(list<XPathExpression>::const_iterator it = xpaths.begin();
+    it != xpaths.end(); it++) {
+    Element * xpath_elem = root->add_child("XPath","psx");
+    xpath_elem->set_attribute("exp",it->expression);
+    for(Node::PrefixNsMap::const_iterator pns = it->ns.begin();
+      pns != it->ns.end(); pns++) {
+      Element * ns_elem = xpath_elem->add_child("Namespace","psx");
+      ns_elem->set_attribute("prefix",pns->first);
+      ns_elem->add_child_text(pns->second);
+    }
+  }
+  _protocol.encode(&doc);
+  _send_socket_io();
+}
+void Connection::unsubscribe() {
+  list<XPathExpression> blank;
+  subscribe(blank);
+} 
 Connection::~Connection() {
   // unsubscribe
-  //unsubscribe();
+  unsubscribe();
+  close(_fd);
+}
+
+list<boost::shared_ptr<Document> > Connection::run(unsigned int usec) {
+  struct timeval tv = { 0 , usec };
+  while ( tv.tv_usec >= 1000000) {
+    tv.tv_sec++;
+    tv.tv_usec-=1000000;
+  }
+  fd_set read, exception;
+  list<shared_ptr<Document> > docs;
+  bool timeout = false;
+  while(docs.empty() || !timeout) {
+    FD_ZERO(&read);
+    FD_ZERO(&exception);
+    FD_SET(_fd,&read);
+    FD_SET(_fd,&exception);
+    int ret = 0;
+    if(usec != 0)
+      ret = select(_fd+1,&read,NULL,&exception,&tv);
+    else
+      ret = select(_fd+1,&read,NULL,&exception,NULL);
+    assert(ret >=0);
+    if (ret == 0)
+      timeout=true;
+    if(FD_ISSET(_fd,&read)) {
+      char buf[1024*64];
+      ssize_t bytes_read = recv(_fd,buf,1024*64,0);
+      vector<shared_ptr<Document> ds = _protocol.decode(buf,bytes_read);
+      //for(unsigned int i = 0; i < ds.size(); i++) {
+      //  
+      //}
+    }
+  }
+  return docs;
 }
