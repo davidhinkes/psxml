d3b172857f7b2ee90b1c0ccb055b2b466ed5f57f
David Hinkes <dh83@localhost.(none)>
2009-02-19 22:30:45 -0800

    test client

--- src/test-client.cc
+++ src/test-client.cc
@@ -8,11 +8,24 @@ using namespace psxml;
 using namespace xmlpp;
 using namespace boost;
 int main() {
-  Connection con("127.0.0.1",10000);
+  Connection con("localhost",10000);
+  list<XPathExpression> exes;
+  XPathExpression ex;
+  ex.expression = "/x:testdata";
+  ex.ns["x"]="http://playboy.com";
+  exes.push_back(ex);
+  con.subscribe(exes);
   list<Element *> l;
   Document d;
-  Element * e =d.create_root_node("testdata");
+  Element * e =d.create_root_node("testdata","http://playboy.com","la");
   l.push_back(e);
   con.publish(l);
+  con.publish(l);
+  con.publish(l);
+  list<Element *> elems = con.run();
+  for(list<Element*>::const_iterator it = elems.begin();
+    it != elems.end(); it++) {
+    cout<<(*it)->get_name()<<endl;
+  }
   return 0;
 }
