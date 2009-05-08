7c09860cea6ef6f669eb473ffeac28700ddd565b
David Hinkes <david.hinkes@gmail.com>
2009-05-07 18:22:18 -0700

    updated tests, new tests dir

--- src/test-client.cc
+++ src/test-client.cc
@@ -16,26 +16,34 @@ int main() {
   ex.ns["x"]="http://playboy.com";
   exes.push_back(ex);
   exes.push_back(ex);
+
+  // subscribe to data
   con.subscribe(exes);
   list<Element *> l;
   Document d;
   Element * e =d.create_root_node("testdata","http://playboy.com","la");
-  unsigned int size = 1024;
+  unsigned int size = 512*1024;
   char buf[size];
   for(unsigned int i=0;i<size;i++)
     buf[i]=10;
   buf[size-1]=0;
   e->add_child_text(buf);
   l.push_back(e);
-  while(true) {
-  list<Element *> elems = con.run(1*1000000);
-  for(list<Element*>::const_iterator it = elems.begin();
-    it != elems.end(); it++) {
-    cout<<(*it)->get_name()<<endl;
-  }
-  if(elems.size() == 0) {
-    con.publish(l);
-  }
-  }
+
+  
+  for(unsigned int s = 0; s < 100; s++) {
+    list<Element *> elems = con.run(1*1000000);
+    if(elems.size() == 0) {
+      // didn't get anything ... bad
+      cerr<<"test-client didn't get exptected publish data"<<endl;
+      for(unsigned int m = 0; m < 64; m++)
+        con.publish(l);
+
+    }
+    for(list<Element*>::const_iterator it = elems.begin();
+      it != elems.end(); it++) {
+      cout<<(*it)->get_name()<<endl;
+    }
+   }
   return 0;
 }
